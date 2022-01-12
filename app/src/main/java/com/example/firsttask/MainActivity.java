68683ea676269664f6694fab8d0013393a4e4bf6
test <ss1503@bs.amalnet.k12.il>
2022-01-12 08:27:30 +0200

    edit

--- app/src/main/java/com/example/firsttask/MainActivity.java
+++ app/src/main/java/com/example/firsttask/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.firsttask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCount = findViewById(R.id.btnClickCounter);
+    }
+
+
+    public void click(View view) {
+        btnCount.setText("Oh yeah!, ive been clicked");
     }
 }
